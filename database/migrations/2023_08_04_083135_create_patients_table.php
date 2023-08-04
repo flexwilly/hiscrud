cc28dc372beedc0793815b697e4d63c3357489bc
Wilson Onjore <onjorew@gmail.com>
2023-08-04 14:28:25 +0300

    Another Commit

--- database/migrations/2023_08_04_083135_create_patients_table.php
+++ database/migrations/2023_08_04_083135_create_patients_table.php
@@ -14,23 +14,21 @@ return new class extends Migration
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
             $table->string('telephone');
-$table->string('patient_name');
-$table->date('patient_dob');
-$table->string('patient_idno');
-$table->string('patient_address');
-$table->string('patient_county');
-$table->string('sub_county');
-$table->string('patient_email');
-$table->string('patient_gender');
-$table->string('patient_marital_status');
-$table->string('kin_name');
-$table->date('kin_dob');
-$table->string('kin_idno');
-$table->string('kin_gender');
-$table->string('kin_relationship');
-$table->string('kin_telephone');
-
-
+            $table->string('patient_name');
+            $table->date('patient_dob');
+            $table->string('patient_idno');
+            $table->string('patient_address');
+            $table->string('patient_county');
+            $table->string('sub_county');
+            $table->string('patient_email');
+            $table->string('patient_gender');
+            $table->string('patient_marital_status');
+            $table->string('kin_name');
+            $table->date('kin_dob');
+            $table->string('kin_idno');
+            $table->string('kin_gender');
+            $table->string('kin_relationship');
+            $table->string('kin_telephone');
             $table->timestamps();
         });
     }
